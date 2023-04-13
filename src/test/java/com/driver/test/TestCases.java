642ed2aa9623d8d47e90f90e47ffd39fe40fb57a
MdAsif9 <115995916+MdAsif9@users.noreply.github.com>
2023-04-14 03:32:42 +0530

    init

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,19 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+       String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class TestCases {
-    
+    public static void main(String args[]){
+        B obj=new B();   
+        System.out.println(obj.meth);
+    }
 }
