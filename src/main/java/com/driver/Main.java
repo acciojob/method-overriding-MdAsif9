dada138f4a10e541b967cf12af776d72a39bc87f
asifk <19btcs001hy@manuu.edu.in>
2023-04-15 20:04:36 +0530

    init

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,6 @@
 package com.driver;
-class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-       String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
+
 public class Main {
   public static void main(String args[]){
    B ans=new B();
