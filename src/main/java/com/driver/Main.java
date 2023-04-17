914ee02d70f7c0e05099ee9639c654034d9cf778
asifk <19btcs001hy@manuu.edu.in>
2023-04-17 18:01:54 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,22 @@
 package com.driver;
 
 public class Main {
-  public static void main(String args[]){
-      A ans=new B();
+    public static class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A {
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
 
-    System.out.println(ans.meth());
+    public static void main(String args[]){
+      B ans=new B();
+
+        ans.meth();
+        B ans1 =new B();
+        ans1.meth();
   }
 }
